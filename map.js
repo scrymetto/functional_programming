2eb60c0f417dfb47d9a8968a26965cdff298ca51
Olya Mityukhina <olga.mityukhina@semrush.com>
2024-01-19 20:13:40 +0100

    another way after check

--- map.js
+++ map.js
@@ -15,3 +15,10 @@ const map = (array, fn) => {
 }
 
 console.log(map([1, 2, 3, 4, 5], add(1)))
+
+const map1 = ([x, ...xs], fn) => {
+  if (!x) return [];
+  return [fn(x), ...map1(xs, fn)]
+}
+
+console.log(map1( [1, 2, 3, 4, 5], add(2)))
