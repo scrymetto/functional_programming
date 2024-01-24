8adc703dc11b792b3c84bdb6eef38b7c3153a15c
Olya Mityukhina <olga.mityukhina@semrush.com>
2024-01-24 10:51:16 +0100

    right reduce

--- reduce.js
+++ reduce.js
@@ -11,11 +11,25 @@ const reduceLeft = (fn, [x, ...xs], initVal) => {
     ? initVal
     : reduceLeft(fn, xs, fn(initVal, x))
 }
-console.log(reduceLeft(sum, [1, 2, 3, 4], 0))
-console.log(reduceLeft(switchOn, ['light', 'water', 'gas', 'pain'], {}))
+// console.log(reduceLeft((acc, item) => [item, ...acc], [1, 2, 3, 4], []))
+// console.log(reduceLeft(switchOn, ['light', 'water', 'gas', 'pain'], {}))
 
-// const reduceRight = (fn, array, result) => {
-// }
-// console.log(reduceRight(sum, [1, 2, 3, 4]))
+const reduceRight = (fn, array, initVal) => {
+  const reduce = (fun, wholeArray, initValue, ind) => {
+    if (ind < 0) return initValue
+    return reduce(fun, wholeArray, fun(initValue, wholeArray[ind]), --ind)
+  }
+
+  return reduce(fn, array, initVal, array.length - 1)
+}
+// console.log(reduceRight(sum, [1, 2, 3, 4], 0))
 // console.log(reduceRight(switchOn, ['light', 'water', 'gas', 'pain'], {}))
 
+const reduceRight2 = (fn, [x, ...xs], initVal) => {
+  return x == null
+    ? initVal
+    : fn(reduceRight2(fn, xs, initVal), x)
+}
+// console.log(reduceRight2(sum, [1, 2, 3, 4], 0))
+// console.log(reduceLeft((acc, item) => [item, ...acc], [1, 2, 3, 4], []))
+// console.log(reduceRight2(switchOn, ['light', 'water', 'gas', 'pain'], {}))
