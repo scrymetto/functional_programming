b0b52596863a0cc121df119ec49cfb2b02709aa5
Olya Mityukhina <olga.mityukhina@semrush.com>
2024-01-24 11:10:22 +0100

    filter with reduce

--- filter.js
+++ filter.js
@@ -10,5 +10,17 @@ const filter = ([x, ...xs], predicate) => {
     ? [x, ...filter(xs, predicate)]
     : filter(xs, predicate)
 }
-console.log(filter([1, 2, 3, 4, 5], predicate))
+// console.log(filter([1, 2, 3, 4, 5], predicate))
 
+const reduceLeft = (fn, [x, ...xs], initVal) => {
+  return x == null
+    ? initVal
+    : reduceLeft(fn, xs, fn(initVal, x))
+}
+
+const filterR = (fn, [x, ...xs]) => {
+  if (x == null) return []
+  const newFunc = (fn) => (acc, curr) => fn(curr) ? [...acc, curr] : acc
+  return reduceLeft(newFunc(fn), xs, [])
+}
+console.log(filterR(predicate, [1, 2, 3, 4, 5]))
